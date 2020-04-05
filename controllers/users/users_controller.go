029eef0e8fd80349110012598b7e6ed5fb0a3e3c
macrusal <macrusal@gmail.com>
2020-04-05 10:17:40 -0300

    dao structure

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/macrusal/bookstore_users-api/services"
 	"github.com/macrusal/bookstore_users-api/utils/errors"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
@@ -25,7 +26,19 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"),10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("User id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
 
 //func SearchUser(c *gin.Context) {
--- services/users_services.go
+++ services/users_services.go
@@ -3,15 +3,22 @@ package services
 import (
 	"github.com/macrusal/bookstore_users-api/domain/users"
 	"github.com/macrusal/bookstore_users-api/utils/errors"
-	"net/http"
 )
 
-func CreateUser(user users.User) (*users.User, *errors.RestErr) {
-	return nil, nil
-
-	return &user, nil
-
-	return &user, &errors.RestErr{
-		Status:  http.StatusInternalServerError,
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	result := &users.User{Id:userId}
+	if err := result.Get(); err != nil {
+		return nil, err
 	}
+	return result, nil
+}
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
