b3815951b84530556f7ac1dbb69a60b117a2c10d
macrusal <macrusal@gmail.com>
2020-04-04 22:10:40 -0300

    creating struct user domain, struct to errors

--- services/users_services.go
+++ services/users_services.go
@@ -1,1 +1,17 @@
 package services
+
+import (
+	"github.com/macrusal/bookstore_users-api/domain/users"
+	"github.com/macrusal/bookstore_users-api/utils/errors"
+	"net/http"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return nil, nil
+
+	return &user, nil
+
+	return &user, &errors.RestErr{
+		Status:  http.StatusInternalServerError,
+	}
+}
