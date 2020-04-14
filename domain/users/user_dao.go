dd0eb5175df0d60081d6309665fe67489b7ffd8d
macrusal <macrusal@gmail.com>
2020-04-13 22:37:47 -0300

    Getting user from users_db/users

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -9,8 +9,10 @@ import (
 )
 
 const (
+	errorsNoRows     = "no rows in result set"
 	indexUniqueEmail = "email"
-	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?)"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?)"
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?"
 )
 
 var (
@@ -18,21 +20,18 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("User %d not found", user.Id))
+	defer stmt.Close()
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorsNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d does not found:", user.Id))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()))
 	}
-
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
-
 	return nil
 }
 
@@ -49,7 +48,7 @@ func (user *User) Save() *errors.RestErr {
 	user.DateCreated = date_utils.GetNowString()
 	insertRasult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
 	if err != nil {
-		if strings.Contains(err.Error(), indexUniqueEmail){
+		if strings.Contains(err.Error(), indexUniqueEmail) {
 			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
 		}
 		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
